d88e401eb2f2456b1bfa7e3c846b8aaa51f0b3c6
salilvnair <salilvnair@github.com>
2022-07-23 01:47:01 -0500

    code clean up

--- src/main/java/com/salilvnair/psvprocessor/test/DummyTest.java
+++ src/main/java/com/salilvnair/psvprocessor/test/DummyTest.java
@@ -1,15 +1,12 @@
 package com.salilvnair.psvprocessor.test;
 
-import com.salilvnair.psvprocessor.service.PsvFileReader;
-import com.salilvnair.psvprocessor.service.PsvFileReaderImpl;
-import com.salilvnair.psvprocessor.util.FileUtils;
-import com.salilvnair.psvprocessor.util.StopWatch;
-
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
+import com.salilvnair.psvprocessor.service.PsvFileReader;
+import com.salilvnair.psvprocessor.service.PsvFileReaderImpl;
+import com.salilvnair.psvprocessor.util.StopWatch;
 
 public class DummyTest {
 
@@ -17,35 +14,26 @@ public class DummyTest {
         PsvFileReader psvFileReader = new PsvFileReaderImpl();
 
         File file = new File("/Users/salilvnair/Workspace/experiments/psvprocessor/src/main/resources/test_pipe.txt");
-        List<DummyClass> megaDummyClasses = new ArrayList<>();
-        if(FileUtils.fileSize(file, FileUtils.SizeUnit.GB) > 1) {
-            //process very large file
-            long totalTime = 0;
-            int chunkSize = 1000000;
-            int chunkMaxValue = 100;
-            for (int i = 0; i < chunkMaxValue; i++) {
-                long from = 1 + (chunkSize*i);
-                long to  = from + (chunkSize-1);
-                System.out.println("from:"+from);
-                System.out.println("to:"+to);
-                StopWatch.start();
-                List<DummyClass> dummyClasses = psvFileReader.read(file, DummyClass.class, from, to);
-                long endTime = StopWatch.elapsed(TimeUnit.SECONDS);
-                totalTime = totalTime + endTime;
-                System.out.println("read "+dummyClasses.size()+" records in "+endTime+ " seconds.");
-                if(chunkSize != dummyClasses.size()) {
-                    System.out.println("reading completed in "+totalTime+" seconds.Going to exit the process...");
-                    dummyClasses.clear();
-                    break;
-                }
+        long totalTime = 0;
+        int chunkSize = 1000000;
+        int chunkMaxValue = 100;
+        for (int i = 0; i < chunkMaxValue; i++) {
+            long from = 1 + (chunkSize*i);
+            long to  = from + (chunkSize-1);
+            System.out.println("from:"+from);
+            System.out.println("to:"+to);
+            StopWatch.start();
+            List<DummyClass> dummyClasses = psvFileReader.read(file, DummyClass.class, from, to);
+            long endTime = StopWatch.elapsed(TimeUnit.SECONDS);
+            totalTime = totalTime + endTime;
+            System.out.println("read "+dummyClasses.size()+" records in "+endTime+ " seconds.");
+            if(chunkSize != dummyClasses.size()) {
+                System.out.println("reading completed in "+totalTime+" seconds.Going to exit the process...");
                 dummyClasses.clear();
-                System.out.println();
+                break;
             }
-        }
-        else {
-            //process large but ok file
-            List<DummyClass> dummyClasses = psvFileReader.read(file, DummyClass.class);
-            System.out.println("read "+dummyClasses.size()+" records in "+StopWatch.elapsed(TimeUnit.SECONDS)+ " seconds.");
+            dummyClasses.clear();
+            System.out.println();
         }
     }
 
